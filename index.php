0028aa141f7a7a98b832e0da18bc0b3ed5455d49
tegaraddict <79796381+tegaraddict@users.noreply.github.com>
2021-02-28 23:08:26 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+* {
+  box-sizing: border-box;
+}
+.menu {
+  float: left;
+  width: 20%;
+}
+.menuitem {
+  padding: 5px;
+  margin-top: 7px;
+  border-bottom: 2px solid #bde0fe;
+}
+.main {
+  float: right;
+  width: 50%
+  padding: 0 200px;
+  overflow: ;
+}
+.right1 {
+  background-color: #a2d2ff;
+  float: left;
+  width: 49%;
+  padding: 10px 15px;
+  margin-top: 7px;
+}
+.right {
+  background-color: #ffafcc;
+  float: right;
+  width: 49%;
+  padding: 10px 15px;
+  margin-top: 7px;
+}
+@media only screen and (max-width:800px) {
+  /* For tablets: */
+  .main {
+    width: 80%;
+    padding: 0;
+  }
+  .right {
+    width: 100%;
+  }
+}
+@media only screen and (max-width:500px) {
+  /* For mobile phones: */
+  .menu, .main, .right {
+    width: 100%;
+  }
+}
+</style>
+</head>
+<body style="font-family:Verdana;">
+
+<div style="background-color:#ffafcc;padding:20px;">
+  <h1>Aditya Tegar Bimantara</h1>
+  <h3>Portofolio</h3>
+</div>
+
+<div style="overflow:auto">
+  <div class="menu">
+    <div class="menuitem"><p><a href="https://wa.me/6285231908700">Whatsapp</a></p></div>
+    <div class="menuitem"><p><a href="https://www.instagram.com/tegaraaditya/">Instagram</a></p>
+</div>
+    <div class="menuitem">History</div>
+    <div class="menuitem">Gallery</div>
+  </div>
+
+  <div class="main">
+    <h2>Curiculum Vitae</h2>
+    <p>Ini adalah portofolio html pertama saya, 28febuari2021</p>
+    <img src="https://pemmzchannel.com/wp-content/uploads/2020/05/Valorant-2.jpg" style="width:100%">
+  </div>
+
+  <div class="right">
+    <h4>Telepon:</h4>085231908700
+    <h4>Email:</h4>ategar765@gmail.com
+    <h4>Linkid:</h4>-
+    <h4>Website:</h4>https://github.com/tegaraddict
+    
+  </div>
+</div><div class="right1">
+    <h4>Tempat, tanggal lahir:</h4>Blitar,29 November 2001
+    <h4>Jenis Kelamin:</h4><p>Laki-Laki.</p>
+    <h4>Agama:</h4><p>Kristen</p>
+    <h4>Status:</h4><p>Pelajar</p>
+    
+  </div>
+</div>
+
+
+<div style="background-color:#f1f1f1;text-align:center;padding:10px;margin-top:7px;font-size:12px;"> Sekian dan terimakasih :).</div>
+
+</body>
+</html>
