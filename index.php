16f35b9ddb6818f455b67cec4cae6ece0b3e94ac
tegaraddict <79796381+tegaraddict@users.noreply.github.com>
2021-03-01 17:45:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
   box-sizing: border-box;
 }
 .menu {
-  float: left;
-  width: 20%;
+  float: LEFT;
+  width: 30%;
 }
 .menuitem {
   padding: 5px;
@@ -48,7 +48,7 @@
 @media only screen and (max-width:500px) {
   /* For mobile phones: */
   .menu, .main, .right {
-    width: 100%;
+    width: 50;
   }
 }
 </style>
@@ -62,23 +62,21 @@
 
 <div style="overflow:auto">
   <div class="menu">
-    <div class="menuitem"><p><a href="https://wa.me/6285231908700">Whatsapp</a></p></div>
-    <div class="menuitem"><p><a href="https://www.instagram.com/tegaraaditya/">Instagram</a></p>
+    <div class="menuitem"><p><a href="https://wa.me/6285231908700"><img src="https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo-whatsapp-128.png" style="width:50%"></a></p></div>
+    <div class="menuitem"><p><a href="https://www.instagram.com/tegaraaditya/"><img src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Instagram-128.png"style="width:50%"></a></p>
 </div>
     <div class="menuitem">History</div>
     <div class="menuitem">Gallery</div>
   </div>
-
   <div class="main">
     <h2>Curiculum Vitae</h2>
     <p>Ini adalah portofolio html pertama saya, 28febuari2021</p>
     <img src="https://pemmzchannel.com/wp-content/uploads/2020/05/Valorant-2.jpg" style="width:100%">
   </div>
-
   <div class="right">
     <h4>Telepon:</h4>085231908700
     <h4>Email:</h4>ategar765@gmail.com
-    <h4>Linkid:</h4>-
+    <h4>LinkIn:</h4>-
     <h4>Website:</h4>https://github.com/tegaraddict
     
   </div>
@@ -89,10 +87,11 @@
     <h4>Status:</h4><p>Pelajar</p>
     
   </div>
+  
 </div>
 
 
-<div style="background-color:#f1f1f1;text-align:center;padding:10px;margin-top:7px;font-size:12px;"> Sekian dan terimakasih :).</div>
+<div style="background-color:#f1f1f1;text-align:CENTER;padding:10px;margin-top:7px;font-size:12px;"> Sekian dan terimakasih :).</div>
 
 </body>
 </html>
